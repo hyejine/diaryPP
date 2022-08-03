3a2fe93d3301c6a784188bb67ebcc55352387d6c
hyejin <rnlcksg0@gmail.com>
2022-08-04 00:11:32 +0900

    네이버 토근 전달

--- demo/src/main/frontend/src/components/login/Login.jsx
+++ demo/src/main/frontend/src/components/login/Login.jsx
@@ -11,42 +11,50 @@ const Login = () => {
   const onNaverLogin = () => {
       const naverLogin = new naver.LoginWithNaverId({
         clientId: "o1yjLGVlc1wfFxCIIGHG",
-        callbackUrl: "http://localhost:3000/naver", 
-        isPopup: true, // popup 형식으로 띄울것인지 설정
+        callbackUrl: "http://localhost:3000/login", 
+        isPopup: false, // popup 형식으로 띄울것인지 설정
         loginButton: { color: 'green', type: 1, height: '47' }, //버튼의 스타일, 타입, 크기를 지정
+        callbackHandle: true
       });
       naverLogin.init();
 
   
-    //   naverLogin.getLoginStatus(async function (status) {
-    //     if (status) {
-    //             // 아래처럼 선택하여 추출이 가능하고, 
-    //       setUserId(naverLogin.user);
-    //             // 정보 전체를 아래처럼 state 에 저장하여 추출하여 사용가능하다. 
-    //             // setUserInfo(naverLogin.user)
+      // window.addEventListener('load', function(){
+        naverLogin.getLoginStatus(async function (status) {
+          console.log(status);
+        if (status) {
+                // 아래처럼 선택하여 추출이 가능하고, 
+          setUserId(naverLogin.user);
+                // 정보 전체를 아래처럼 state 에 저장하여 추출하여 사용가능하다. 
+                // setUserInfo(naverLogin.user)
               
-    //           }
-    //         }) 
-    //         console.log(naverLogin.user);
+              }
+            }) 
+            // console.log(userid);
+          // })
     };
-    // console.log(userid);
+    console.log(userid);
 
-    const location = useLocation();  
-
-    const getNaverToken = () => {
-      if (!location.hash) return;
-      const token = location.hash.split('=')[1].split('&')[0];
-      console.log(token);
-    };
-
+    const userAccessToken = () => {
+      window.location.href.includes('access_token') && getToken()
+}
       
-    // 요기!
-	
+      const getToken = () => {
+  const token = window.location.href.split('=')[1].split('&')[0]
+  // .split('=')[1].split('&')[0]
+           // console.log, alert 창을 통해 토큰이 잘 추출 되는지 확인하자! 
+          console.log(token);
+           // 이후 로컬 스토리지 또는 state에 저장하여 사용하자!   
+              localStorage.setItem('access_token', token)
+          // setGetToken(token)
+}
 
-    useEffect(() => {
-      onNaverLogin();
-      getNaverToken();
-    }, []);
+      // console.log(window.location.href);
+           // 화면 첫 렌더링이후 바로 실행하기 위해 useEffect 를 사용하였다.
+useEffect(() => {
+  onNaverLogin()
+  userAccessToken()
+}, [])
   return (
     <div className="login_wrap">
       <div className="login_inner">
