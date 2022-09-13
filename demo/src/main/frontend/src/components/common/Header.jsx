b4848d3067b715246a5e736bd75834d289a0ce3b
hyejin <rnlcksg0@gmail.com>
2022-09-13 22:36:19 +0900

    header 설정

--- demo/src/main/frontend/src/components/common/Header.jsx
+++ demo/src/main/frontend/src/components/common/Header.jsx
@@ -1,25 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.scss';
-import DeleteIcon from '@mui/icons-material/Delete';
-// import {  AccessAlarm } from '@mui/icons-material';
+import { Button, Menu, MenuItem, Fade } from '@mui/material';
+import { SettingsSuggestOutlined,  AccountCircle, KeyboardArrowDown} from '@mui/icons-material';
+import { useState } from 'react';
 
 const Header =(props)=>{
   const {currnetUser} = props;
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    console.log(event.currentTarget);
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   console.log(currnetUser);
         return (
           <div className="header">
             <Link to="/"><span className="logo">LOGO</span></Link>
-            {currnetUser.email ?  
-            <div>
-              {currnetUser.image ? currnetUser.image:<></>}
-              {currnetUser.name}
-              <DeleteIcon/>
+            {/* {currnetUser.email ?   */}
+            <div className="login">
+              <Button
+        onClick={handleClick}
+      >
+          {currnetUser.image ? currnetUser.image:<AccountCircle className="" color="disabled"/>}
+              {currnetUser.name }정혜진
+       <KeyboardArrowDown className="arrowDowun"/>
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem >Language</MenuItem>
+        <MenuItem >Font</MenuItem>
+        <MenuItem >Background Color</MenuItem>
+        <MenuItem >Logout</MenuItem>
+        <MenuItem >프리미엄 이모지 보러가기</MenuItem>
+      </Menu>
+                    
+              <SettingsSuggestOutlined/>
             </div>
-            : 
+            {/* : 
             <Link to="/auth/login"><span className="login">login</span></Link>
-          }
+          } */}
           </div>
         );
 }
