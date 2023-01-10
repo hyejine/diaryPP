45998eb4bf9623a234854a07b7623494961de2c0
hyejine <hyejine1997@naver.com>
2023-01-10 18:01:15 +0900

    네이버 로그인 진행 중

--- demo/src/main/frontend/src/components/login/Login.jsx
+++ demo/src/main/frontend/src/components/login/Login.jsx
@@ -68,53 +68,53 @@ const Login = (props) => {
         setLoginErr(true);
       });
   };
-  // const onNaverLogin = () => {
-  //   const naverLogin = new naver.LoginWithNaverId({
-  //     clientId: "o1yjLGVlc1wfFxCIIGHG",
-  //     callbackUrl: "http://localhost:3000/login",
-  //     isPopup: false, // popup 형식으로 띄울것인지 설정
-  //     loginButton: { color: "green", type: 1, height: "47" }, //버튼의 스타일, 타입, 크기를 지정
-  //     callbackHandle: true,
-  //   });
-  //   naverLogin.init();
+  const onNaverLogin = () => {
+    const naverLogin = new naver.LoginWithNaverId({
+      clientId: "o1yjLGVlc1wfFxCIIGHG",
+      callbackUrl: "http://localhost:3000/user/login",
+      isPopup: false, // popup 형식으로 띄울것인지 설정
+      loginButton: { color: "green", type: 1, height: "47" }, //버튼의 스타일, 타입, 크기를 지정
+      callbackHandle: true,
+    });
+    naverLogin.init();
 
-  //   // window.addEventListener('load', function(){
-  //   naverLogin.getLoginStatus(async function (status) {
-  //     console.log(status);
-  //     if (status) {
-  //       setUserId(naverLogin.user);
-  //     }
-  //   });
-  //   // })
-  // };
-  // console.log(userid);
+    // window.addEventListener('load', function(){
+    naverLogin.getLoginStatus(async function (status) {
+      console.log(status);
+      if (status) {
+        setUserId(naverLogin.user);
+      }
+    });
+    // })
+  };
+  console.log(userid);
 
-  // const userAccessToken = () => {
-  //   // window.location.href.includes("access_token") && getToken();
-  // };
+  const userAccessToken = () => {
+    window.location.href.includes("access_token") && getNaverToken();
+  };
 
-  // const getToken = () => {
-  //   const token = window.location.href.split("=")[1].split("&")[0];
-  // //   axios.post(`https://openapi.naver.com/v1/nid/me`, {
-  // //     token
-  // // }, {
-  // //     withCredentials: true
-  // // })
-  // // .then((res)=> {
-  // //     window.location.replace('/')
-  // //   //서버측에서 로직이 완료되면 홈으로 보내준다
-  // // })
+  const getNaverToken = () => {
+    const token = window.location.href.split("=")[1].split("&")[0];
+  //   axios.post(`https://openapi.naver.com/v1/nid/me`, {
+  //     token
+  // }, {
+  //     withCredentials: true
+  // })
+  // .then((res)=> {
+  //     window.location.replace('/')
+  //   //서버측에서 로직이 완료되면 홈으로 보내준다
+  // })
 
-  //   console.log(token);
-  //   // 이후 로컬 스토리지 또는 state에 저장하여 사용하자!
-  //   localStorage.setItem("access_token", token);
-  //   // setGetToken(token)
-  // };
+    console.log(token);
+    // 이후 로컬 스토리지 또는 state에 저장하여 사용하자!
+    localStorage.setItem("access_token", token);
+    // setGetToken(token)
+  };
 
-  // useEffect(() => {
-  //   onNaverLogin();
-  //   userAccessToken();
-  // }, []);
+  useEffect(() => {
+    onNaverLogin();
+    userAccessToken();
+  }, []);
   const grant_type = "authorization_code";
   const REST_API_KEY = "380089a4f363b679b2dbe89f5bed98ae";
   const REDIRECT_URI = "http://localhost:3000/login";
